c4777c250fec1bd4d31bf347682453cf21203ab3
Tom Settle <tsettle44@gmail.com>
2019-01-09 17:05:19 -0500

    axios get posts

--- client/src/components/pages/DiscussionRoom.js
+++ client/src/components/pages/DiscussionRoom.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class DiscussionRoom extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Discussion Room</h1>
-      </div>
-    )
-  }
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/posts')
+        .then(res => this.setState({ posts: res.data }))
+    }
+
+    render() {
+        return (
+            <div>
+            <h1>Discussion Room</h1>
+            </div>
+        )
+    }
 }
 
 export default DiscussionRoom
