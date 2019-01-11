d5126767b899266b8aa7657c4bdcc5453feec380
Tom Settle <tsettle44@gmail.com>
2019-01-11 18:21:38 -0500

    body buttons change with login

--- client/src/App.js
+++ client/src/App.js
@@ -8,72 +8,74 @@ import Login from "./components/auth/Login";
 import DiscussionRoom from "./components/pages/DiscussionRoom";
 import axios from "axios";
 import Profile from "./components/pages/Profile";
-import { Security, ImplicitCallback } from '@okta/okta-react'
+import { Security, ImplicitCallback } from "@okta/okta-react";
+import SecureRoute from "@okta/okta-react/dist/SecureRoute";
 
 const config = {
-  issuer: 'https://dev-612249.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oaiwik3abIBUoTca0h7'
-}
+  issuer: "https://dev-612249.oktapreview.com/oauth2/default",
+  redirect_uri: window.location.origin + "/implicit/callback",
+  client_id: "0oaiwik3abIBUoTca0h7"
+};
 
 function onAuthRequired({ history }) {
-  history.push('/login');
+  history.push("/login");
 }
 
 class App extends Component {
-
-  state = {
-    authed: false,
-  }
-
-  SignUp = (state) => {
-    
+  SignUp = state => {
     const { firstName, lastName, email, password, confirmPassword } = state;
 
     const self = this;
-    
-    if(password !== confirmPassword) {
-        console.log("Passwords do not match")
+
+    if (password !== confirmPassword) {
+      console.log("Passwords do not match");
     } else {
-        axios.post('http://localhost:5000/api/test/users', {
-            firstName,
-            lastName,
-            email,
-            password, 
-            confirmPassword
+      axios
+        .post("http://localhost:5000/api/test/users", {
+          firstName,
+          lastName,
+          email,
+          password,
+          confirmPassword
         })
-        .then(function (response) {
-            console.log(response);
-            self.setState({authed: true});
-            //window.location = '/sign-up/success';
+        .then(function(response) {
+          console.log(response);
+          self.setState({ authed: true });
+          //window.location = '/sign-up/success';
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch(function(error) {
+          console.log(error);
         });
     }
-  } 
+  };
 
   render() {
     return (
       <Router>
-        <Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri}
-                  onAuthRequired={onAuthRequired}
+        <Security
+          issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+          onAuthRequired={onAuthRequired}
         >
           <div>
             <NavBar />
-            <Route exact path="/" component={Body}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route path="/discussion-room" component={DiscussionRoom}></Route>
-            <Route exact path="/sign-up" render={(props) => <SignUp {...props} SignUp={this.SignUp} /> } />
-            <Route 
-              path="/login" 
+            <Route exact path="/" component={Body} />
+            <Route path="/about" component={About} />
+            <Route path="/discussion-room" component={DiscussionRoom} />
+            <Route
+              exact
+              path="/sign-up"
+              render={props => <SignUp {...props} SignUp={this.SignUp} />}
+            />
+            <Route
+              path="/login"
               render={() => (
-                    <Login baseUrl="https://dev-612249.oktapreview.com" />
-                  )}>
-            </Route>
-            <Route path='/implicit/callback' component={ImplicitCallback}/>
+                <Login baseUrl="https://dev-612249.oktapreview.com" />
+              )}
+            />
+            <SecureRoute path="/profile" exact component={Profile} />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
           </div>
         </Security>
       </Router>
--- client/src/components/layout/Body.js
+++ client/src/components/layout/Body.js
@@ -1,35 +1,86 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col, Button } from "reactstrap";
 import { Link } from "react-router-dom";
+import { withAuth } from "@okta/okta-react";
 import "../../styles/body.css";
 
-class Body extends Component{
-    render() {
-        return (
-            <div className="wrapper">
-                <Container className="container">
-                    <div className="headingWrapper">
-                        <Row>
-                            <Col sm="12" md={{ size: 6, offset: 3 }} className="center header"><h1>Welcome to Forum</h1></Col>
-                        </Row>
-                        <Row>
-                            <Col sm="12" md={{ size: 6, offset: 3 }} className="center header"><h3>Sign in or Log in</h3></Col>
-                        </Row>
-                        <Row>
-                            <Col sm="12" md={{ size: 6, offset: 3 }} className="center header">
-                                <Link to="/sign-up">
-                                    <Button className="margin" color="primary">Sign Up</Button>
-                                </Link>
-                                <Link to="login">
-                                    <Button className="margin" color="success">Log in</Button>
-                                </Link>
-                            </Col>
-                        </Row>
-                    </div>
-                </Container>
-            </div>
-        );
-    }
-}
+export default withAuth(
+  class Body extends Component {
+    state = {
+      authenticated: null
+    };
 
-export default Body;
+    checkAuthentication = async () => {
+      const authenticated = await this.props.auth.isAuthenticated();
+      if (authenticated !== this.state.authenticated) {
+        this.setState({ authenticated });
+      }
+    };
+
+    async componentDidMount() {
+      this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+      this.checkAuthentication();
+    }
+
+    login = async () => {
+      // Redirect to '/' after login
+      this.props.auth.login("/");
+    };
+
+    logout = async () => {
+      // Redirect to '/' after logout
+      this.props.auth.logout("/");
+    };
+
+    render() {
+      if (this.state.authenticated === null) return null;
+
+      const button = this.state.authenticated ? (
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }} className="center header">
+            <Link to="/profile">
+              <Button className="btn btn-lg btn-info">My Profile</Button>
+            </Link>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }} className="center header">
+            <Link to="/sign-up">
+              <Button className="margin" color="primary">
+                Sign Up
+              </Button>
+            </Link>
+            <Link to="login">
+              <Button className="margin" color="success">
+                Log in
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      );
+
+      return (
+        <div className="wrapper">
+          <Container className="container">
+            <div className="headingWrapper">
+              <Row>
+                <Col
+                  sm="12"
+                  md={{ size: 6, offset: 3 }}
+                  className="center header"
+                >
+                  <h1>Welcome to Forum</h1>
+                </Col>
+              </Row>
+              {button}
+            </div>
+          </Container>
+        </div>
+      );
+    }
+  }
+);
