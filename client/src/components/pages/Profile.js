d8b31f36641cac0ccc2b9fad696e083b9bc0f9b7
tsettle44 <tsettle44@gmail.com>
2019-01-11 19:20:21 -0500

    add profile props

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React from "react";
+import { withAuth } from "@okta/okta-react";
 
-export class Profile extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Profile</h1>
-      </div>
-    )
+export default withAuth(
+  class ProfilePage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { user: null };
+      this.getCurrentUser = this.getCurrentUser.bind(this);
+    }
+
+    async getCurrentUser() {
+      this.props.auth.getUser().then(user => this.setState({ user }));
+    }
+
+    componentDidMount() {
+      this.getCurrentUser();
+    }
+
+    render() {
+      if (!this.state.user) return null;
+      return (
+        <section className="user-profile">
+          <h1>User Profile</h1>
+          <div>
+            <label>Name:</label>
+            <span>{this.state.user.name}</span>
+          </div>
+        </section>
+      );
+    }
   }
-}
-
-export default Profile
+);
