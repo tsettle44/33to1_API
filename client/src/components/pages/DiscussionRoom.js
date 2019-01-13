8b4876c8f5034587303d3ed56616945e585f1e34
tsettle44 <tsettle44@gmail.com>
2019-01-12 23:30:35 -0500

    render posts to dom

--- client/src/components/pages/DiscussionRoom.js
+++ client/src/components/pages/DiscussionRoom.js
@@ -1,24 +1,38 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export class DiscussionRoom extends Component {
+  state = {
+    posts: []
+  };
 
-    state = {
-        posts: []
-    }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/posts")
+      .then(res => this.setState({ posts: res.data }));
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:5000/api/posts')
-        .then(res => this.setState({ posts: res.data }))
-    }
-
-    render() {
-        return (
-            <div>
-            <h1>Discussion Room</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.state.posts.map(post => (
+          <div>
+            <h3>{post.body}</h3>
+            <h5>{post.createdAt}</h5>
+            <h5>{post.likes}</h5>
+            <h5>{post.name}</h5>
+            {post.comments.map(comment => (
+              <div>
+                <li>{comment.name}</li>
+                <li>{comment.createdAt}</li>
+                <li>{comment.body}</li>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default DiscussionRoom
+export default DiscussionRoom;
