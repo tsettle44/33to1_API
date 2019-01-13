d82af6ca033e7bbc814b2ae5742ab18de4e39eb1
tsettle44 <tsettle44@gmail.com>
2019-01-12 23:37:30 -0500

    fix map key

--- client/src/components/pages/DiscussionRoom.js
+++ client/src/components/pages/DiscussionRoom.js
@@ -15,14 +15,14 @@ export class DiscussionRoom extends Component {
   render() {
     return (
       <div>
-        {this.state.posts.map(post => (
-          <div>
+        {this.state.posts.map((post, i) => (
+          <div key={i}>
             <h3>{post.body}</h3>
             <h5>{post.createdAt}</h5>
             <h5>{post.likes}</h5>
             <h5>{post.name}</h5>
-            {post.comments.map(comment => (
-              <div>
+            {post.comments.map((comment, c) => (
+              <div key={c}>
                 <li>{comment.name}</li>
                 <li>{comment.createdAt}</li>
                 <li>{comment.body}</li>
