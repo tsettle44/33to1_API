b04de6477bd53eccda4a3830b4b09edccc0abffe
Tom Settle <tsettle44@gmail.com>
2019-01-13 19:27:17 -0500

    comment to post mongoose

---
+++ server/models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: []
+});
+
+//Sort Posts
+
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
